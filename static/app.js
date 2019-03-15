b1cab6edbd7a4142ad0d0c693c3d4577ea7aaaa2
SarahHeck <sfheckman@gmail.com>
2019-03-15 14:15:12 -0500

    graphs

--- static/app.js
+++ static/app.js
@@ -3,9 +3,9 @@ function buildMetadata(sample) {
   // @TODO: Complete the following function that builds the metadata panel
 
   // Use `d3.json` to fetch the metadata for a sample
-  // Relates to app
-  var url = `/metadata/${sample}`;
-  d3.json(url).then(function(sample){
+  // Relates to app.py
+  var urlMeta = `/metadata/${sample}`;
+  d3.json(urlMeta).then(function(sample){
     // Use d3 to select the panel with id of `#sample-metadata`
     // Relates to index
       var sampleMetadata = d3.select("#sample-metadata");
@@ -27,14 +27,47 @@ function buildMetadata(sample) {
 
 function buildCharts(sample) {
 
+  var urlCharts = `/samples/${sample}`;
   // @TODO: Use `d3.json` to fetch the sample data for the plots
+  d3.json(urlCharts).then(function(data) {
 
     // @TODO: Build a Bubble Chart using the sample data
+    var trace1 = {
+      x: data.otu_ids,
+      y: data.sample_values,
+      marker: {
+        size: data.sample_values,
+        color: data.sample_values,
+      },
+      text: data.otu_labels
+    }
 
+    var data = [trace1];
+
+    var layout = {
+      xaxis: {
+        title: 'OTU ID',
+      }
+    };
+    
+    Plotly.newPlot('Bubble Chart', data, layout);
+    
     // @TODO: Build a Pie Chart
-    // HINT: You will need to use slice() to grab the top 10 sample_values,
-    // otu_ids, and labels (10 each).
-}
+    var data = data.sample_values.sort((first, second) => second - first);
+    var data = data.slice(0, 10)
+  
+    var trace2 = {
+      labels: data.otu_ids,
+      values: data.sample_values,
+      text: data.otu_labels,
+      type: "pie"
+    };
+    
+    var data = [trace2];
+    
+    Plotly.newPlot("Pie Graph", data);
+  })
+};
 
 function init() {
   // Grab a reference to the dropdown select element
